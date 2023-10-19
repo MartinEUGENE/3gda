474046138c458a6972048784090450b0285b095a
Martin <martinmagicdragon@gmail.com>
2023-10-19 15:36:20 +0200

    raycast

--- Assets/Scripts/CharacterControls.cs
+++ Assets/Scripts/CharacterControls.cs
@@ -4,42 +4,62 @@ using UnityEngine;
 
 public class CharacterControls : MonoBehaviour
 {
-    public float that = 0f;
     public float pitch = 0f;
-
     public float walkspeed = 5.0f;
-    public float sensitivty = 2.0f; 
+    public float sensitivity = 2.0f;
 
-    private Rigidbody rb; 
+    private Rigidbody rb;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        rb = gameObject.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>(); // Corrected line to get the Rigidbody component
+        
     }
 
     void Look()
     {
         pitch = Mathf.Clamp(pitch, -90.0f, 90.0f);
-        pitch -= Input.GetAxisRaw("Mouse Y") * sensitivty;
-        that += Input.GetAxisRaw("Mouse X") * sensitivty;
-        Camera.main.transform.localRotation = Quaternion.Euler(pitch, that, 0);
+        pitch -= Input.GetAxisRaw("Mouse Y") * sensitivity;
+        transform.Rotate(0, Input.GetAxisRaw("Mouse X") * sensitivity, 0);
+        Camera.main.transform.localRotation = Quaternion.Euler(pitch, 0, 0);
     }
+
     void Movement()
     {
-        Vector2 axis = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) *  walkspeed;
-        Vector3 forward = new Vector3(-Camera.main.transform.right.z, 0f, Camera.main.transform.right.x);
-        Vector3 ThatDirection = (forward * axis.x + Camera.main.transform.right * axis.y + Vector3.up * rb.velocity.y);
-        rb.velocity = ThatDirection; 
+        Vector2 axis = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * walkspeed;
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 right = Camera.main.transform.right;
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+        Vector3 ThatDirection = (forward * axis.x + right * axis.y + Vector3.up * rb.velocity.y);
+        rb.velocity = ThatDirection;
+    }
+
+    void Update()
+    {
+        Look();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (!string.IsNullOrEmpty(hit.transform.gameObject.name))
+                {
+                    Debug.Log("Object Name: " + hit.transform.gameObject.name);
+                }
+            }
+        }
     }
 
     private void FixedUpdate()
     {
         Movement();
     }
-
-    void Update()
-    {
-        Look();
-    }
 }
+
