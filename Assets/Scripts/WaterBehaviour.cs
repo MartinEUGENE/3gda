0e50d2a0fc6188f9aece288bbe6fb999ebb03d69
NisemonoQ <yann.badinga@gmail.com>
2023-11-18 11:44:47 +0100

    SD integration et water behaviour reparations
    Les valeurs dans le script de l'eau étaient ma affectées, ça a été remplacé par des float qui peuvent être modifiés.

--- Assets/Scripts/WaterBehaviour.cs
+++ Assets/Scripts/WaterBehaviour.cs
@@ -4,61 +4,81 @@ using UnityEngine;
 
 public class WaterBehaviour : BroColor
 {
-    public float conveyorForce = 10.0f;
-    public bool active = false;
+    public float conveyorForce = 15.0f;     //Pour les cailloux
+    public float smallForce = 12.5f;      //Pour le joueur
+
+
     private BoxCollider woter;
     public FMOD.Studio.EventInstance wa;
-
+    public FMOD.Studio.EventInstance so;
 
     private void Start()
     {
         woter = GetComponent<BoxCollider>();
         woter.isTrigger = false;
         wa = FMODUnity.RuntimeManager.CreateInstance("event:/Mechanics/Water");
+        so = FMODUnity.RuntimeManager.CreateInstance("event:/WaterIn"); 
 
         wa.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
     }
 
 
-
     protected override void CustomActivation()
     {
-        active = true;
-        woter.isTrigger = true;
-       
-        wa.start();
-        
+        isActive = true; 
+        woter.isTrigger = true;       
+        wa.start();        
     }
 
     protected override void CustomDeactivation()
     {
-        active = false;
+        isActive = false; 
         woter.isTrigger = false;
        /* paint.start();
         paint.setParameterByNameWithLabel("Activation", "Active");*/
         wa.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
     }
 
-    
-   /* private void OnTriggerEnter(Collider other)
+
+    /* private void OnTriggerEnter(Collider other)
+     {
+         if (active == true && other.CompareTag("Player"))
+         {
+             other.GetComponent<Rigidbody>().AddForce(transform.forward * conveyorForce);
+         }
+
+     }*/
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (active == true && other.CompareTag("Player"))
+        bool inside = true;
+
+        if (isActive && inside && other.CompareTag("Player"))
         {
-            other.GetComponent<Rigidbody>().AddForce(transform.forward * conveyorForce);
+            inside = false;
+            so.start();
+            Debug.Log("Ga");
         }
-            
-    }*/
+
+
+        if(isActive && inside && other.CompareTag("Rock"))
+        {
+            inside = false;
+            //so.start();
+            Debug.Log("gruuuuuuuuuuuuuuuuu");
+        }
+    }
 
     private void OnTriggerStay(Collider other)
     {
-        if (active == true && other.CompareTag("Player"))
+        if (isActive == true && other.CompareTag("Player"))
         {
-            other.GetComponent<Rigidbody>().AddForce(transform.forward * 12.5f, ForceMode.Force);
+            other.GetComponent<Rigidbody>().AddForce(transform.forward * smallForce, ForceMode.Force);
         }
 
-        if (active == true && other.CompareTag("Rock") && !other.CompareTag("Cloud") && !other.CompareTag("Fog") && !other.CompareTag("Ground"))
+        if (isActive == true && other.CompareTag("Rock") && !other.CompareTag("Cloud") && !other.CompareTag("Fog") && !other.CompareTag("Ground"))
         {
-            other.GetComponent<Rigidbody>().AddForce(transform.forward * 15f, ForceMode.Acceleration);
+            other.GetComponent<Rigidbody>().AddForce(transform.forward * conveyorForce, ForceMode.Acceleration);
         }
 
     }
