d4bcd3a4c929ead90aa3ef9f3e105528463480d5
NisemonoQ <yann.badinga@gmail.com>
2023-12-05 14:42:37 +0100

    Magnet repaired
    L'aimant ne fonctionnait pas à cause de l'ancien système d'héritage, il est maintenant fonctionnel et peut être modifié et régulé.

--- Assets/Scripts/The World behaviours/MagnetBehaviour.cs
+++ Assets/Scripts/The World behaviours/MagnetBehaviour.cs
@@ -7,7 +7,7 @@ public class MagnetBehaviour : BroColor
     //public float raycastDistance = 10f;
     //public float castRadius = 1.0f;
     public float magnetSpeed = .01f;
-    public bool attract = false;
+    //public bool attract = false;
 
     [SerializeField] GameObject metal;
     [SerializeField] GameObject magnet;
@@ -16,34 +16,9 @@ public class MagnetBehaviour : BroColor
     {
         if (isActive == true)
         {
-            //MoveMetalObject();*/
-            metal.transform.position = Vector3.MoveTowards(metal.transform.position, magnet.transform.position, magnetSpeed);
-
-
-            Debug.Log("Yes it works");
-        /*
-
-        RaycastHit[] hits = Physics.SphereCastAll(transform.position, castRadius, transform.forward, raycastDistance);
-
-         // Draw the cast for visualization
-         Debug.DrawRay(transform.position, transform.forward * raycastDistance, Color.blue);
-
-
-         // Check each object hit by the raycast
-         foreach (RaycastHit hit in hits)
-         {
-             // Check if the object has the tag "metal"
-             if (hit.collider.CompareTag("Metal"))
-             {
-
-                 if(attract == true) 
-                 {
-                     Debug.Log("Metal object hit ");
-                     MoveMetalObject(hit.collider.gameObject);
-                 }
-                 // Do something with the metal object, e.g., apply a special effect
-             }*/
-         }
+            MoveMetalObject(metal);
+            Debug.Log("Yes it works");       
+        }
 
 
     }
@@ -58,26 +33,56 @@ public class MagnetBehaviour : BroColor
 
     public override void CustomDeactivation()
     {
-        //isActive = false; 
+        isActive = false; 
         Debug.Log("NO colo ");
     }
 
-    void MoveMetalObject()
+    void MoveMetalObject(GameObject magnetized)
     {
-        
-            metal.transform.position = Vector3.MoveTowards(metal.transform.position, magnet.transform.position, magnetSpeed);
-        
+        if(magnetized.GetComponent<BroColor>().isActive == true)
+        {
+            Vector3 direction = (transform.position - magnetized.transform.position).normalized;
 
+            // Calculate the distance to the magnet
+            float distanceToMagnet = Vector3.Distance(transform.position, magnetized.transform.position);
 
-        // Calculate the direction from the metal object to the object with this script
-       /* Vector3 direction = (transform.position - metalObject.transform.position).normalized;
-
-        // Calculate the distance to the magnet
-        float distanceToMagnet = Vector3.Distance(transform.position, metalObject.transform.position);
-
-        // Move the metal object towards the object with this script with a force proportional to the distance
-        metalObject.GetComponent<Rigidbody>().AddForce(direction * moveSpeed * distanceToMagnet);*/
+            // Move the metal object towards the object with this script with a force proportional to the distance
+            magnetized.GetComponent<Rigidbody>().AddForce(direction * magnetSpeed /* distanceToMagnet*/); 
+        }
     }
 
 
 }
+
+
+/*
+
+       RaycastHit[] hits = Physics.SphereCastAll(transform.position, castRadius, transform.forward, raycastDistance);
+
+        // Draw the cast for visualization
+        Debug.DrawRay(transform.position, transform.forward * raycastDistance, Color.blue);
+
+
+        // Check each object hit by the raycast
+        foreach (RaycastHit hit in hits)
+        {
+            // Check if the object has the tag "metal"
+            if (hit.collider.CompareTag("Metal"))
+            {
+
+                if(attract == true) 
+                {
+                    Debug.Log("Metal object hit ");
+                    MoveMetalObject(hit.collider.gameObject);
+                }
+                // Do something with the metal object, e.g., apply a special effect
+            }*/
+
+        // Calculate the direction from the metal object to the object with this script
+    /* Vector3 direction = (transform.position - metalObject.transform.position).normalized;
+
+    // Calculate the distance to the magnet
+     float distanceToMagnet = Vector3.Distance(transform.position, metalObject.transform.position);
+
+    // Move the metal object towards the object with this script with a force proportional to the distance
+    metalObject.GetComponent<Rigidbody>().AddForce(direction * moveSpeed * distanceToMagnet);*/
