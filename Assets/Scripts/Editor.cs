c6033662bb0ea15ff5656a396ddfa95a7346fef5
Martin <martinmagicdragon@gmail.com>
2024-01-18 16:04:30 +0100

    ON/OFF edit
    appuyer sur E pour activer/ désactiver edit

--- Assets/Scripts/Editor.cs
+++ Assets/Scripts/Editor.cs
@@ -6,31 +6,43 @@ public class Editor : MonoBehaviour
     public GameObject[] stock;
     public GameObject essayer;
     public int objetActif;
+    public bool editOn = false;
 
     void Update()
     {
-        // Increase or decrease objetActif based on the scroll wheel movement
-        objetActif += (int)Input.mouseScrollDelta.y;
+        if (Input.GetKeyDown(KeyCode.E) && editOn == false)
+        {
+            editOn = true;
+        }
+        else
+        {
+            editOn = false;
+        }
 
-        // Ensure objetActif stays within the valid range
+        objetActif += (int)Input.mouseScrollDelta.y;
+        
         objetActif = Mathf.Clamp(objetActif, 0, stock.Length - 1);
 
-        if (Input.GetMouseButtonDown(1))
+        if(editOn == true)
         {
-            objetActif = 0;
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            // Raycast from the camera to the mouse position
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
+            if (Input.GetMouseButtonDown(1))
             {
-                SpawnPrefab(hit.point);
+                objetActif = 0;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    SpawnPrefab(hit.point);
+                }
             }
         }
+        
     }
 
     void SpawnPrefab(Vector3 position)
@@ -43,5 +55,10 @@ public class Editor : MonoBehaviour
         {
             Debug.LogError("Invalid objetActif index.");
         }
+    }
+
+    public void Switch()
+    {
+
     }
 }
