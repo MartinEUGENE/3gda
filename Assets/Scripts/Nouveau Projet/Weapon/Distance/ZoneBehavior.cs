8543f9f838d575f1df0daa88d71a23c580772051
Martin <martinmagicdragon@gmail.com>
2024-03-07 21:32:04 +0100

    weapon start

---
+++ Assets/Scripts/Nouveau Projet/Weapon/Distance/ZoneBehavior.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneBehavior : MonoBehaviour
+{
+    // pain ennemy and die in a few seconds ; pour bombe beam et spray?
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
