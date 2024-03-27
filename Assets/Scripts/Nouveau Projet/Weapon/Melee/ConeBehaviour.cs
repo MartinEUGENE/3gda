8c4b9e32b08bebc15f22e9359938dae0cd522dc3
Martin <martinmagicdragon@gmail.com>
2024-03-27 15:38:57 +0100

    cone done
    le spray est fini

--- Assets/Scripts/Nouveau Projet/Weapon/Melee/ConeBehaviour.cs
+++ Assets/Scripts/Nouveau Projet/Weapon/Melee/ConeBehaviour.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public class ConeBehaviour : MonoBehaviour
 {
-    public float duration = 2f;
-    public float destroyDelay = 1f;
+    public float duration = 0.5f;
+    public float destroyDelay = 0.25f;
     private float timer = 0f;
     private bool destroyScheduled = false;
 
+    //private Rigidbody rigi;
     private Vector3 initialLocalPosition;
 
+    private void Awake()
+    {
+        //rigi = GetComponent<Rigidbody>();
+        //rigi.constraints = RigidbodyConstraints.FreezeRotation;
+    }
     void Start()
     {
         initialLocalPosition = transform.localPosition;
@@ -21,13 +27,13 @@ public class ConeBehaviour : MonoBehaviour
         float progress = Mathf.Clamp01(timer / duration);
 
         // Increase the scale by 2 units in all axes
-        Vector3 newScale = Vector3.one * (1f + 2f * progress);
+        Vector3 newScale = Vector3.one * (0.5f + 1f * progress);
 
         // Set the new scale
         transform.localScale = newScale;
 
         // If the animation is complete, schedule destruction
-        if (progress >= 1f && !destroyScheduled)
+        if (progress >= 0.5f && !destroyScheduled)
         {
             Invoke("DestroyObject", destroyDelay);
             destroyScheduled = true;
