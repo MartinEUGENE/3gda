e30c57ba66349d644f2c50b25bc1204b5f1cf681
Martin <martinmagicdragon@gmail.com>
2024-03-27 14:03:05 +0100

    more cone

--- Assets/Scripts/Nouveau Projet/Weapon/Melee/ConeBehaviour.cs
+++ Assets/Scripts/Nouveau Projet/Weapon/Melee/ConeBehaviour.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ConeBehaviour : MonoBehaviour
 {
-    //  créé une hitbox qui grandi. collé au joueur: suie le mouvement ou pas?
+    public float duration = 2f;
+    public float destroyDelay = 1f;
+    private float timer = 0f;
+    private bool destroyScheduled = false;
+
+    private Vector3 initialLocalPosition;
+
     void Start()
     {
-        
+        initialLocalPosition = transform.localPosition;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        float progress = Mathf.Clamp01(timer / duration);
+
+        // Increase the scale by 2 units in all axes
+        Vector3 newScale = Vector3.one * (1f + 2f * progress);
+
+        // Set the new scale
+        transform.localScale = newScale;
+
+        // If the animation is complete, schedule destruction
+        if (progress >= 1f && !destroyScheduled)
+        {
+            Invoke("DestroyObject", destroyDelay);
+            destroyScheduled = true;
+        }
+    }
+
+    void DestroyObject()
+    {
+        Destroy(gameObject);
     }
 }
