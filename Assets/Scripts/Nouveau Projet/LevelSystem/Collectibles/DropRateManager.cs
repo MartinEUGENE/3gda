f0a0d06ba7055753420d97de3a9ca4375d0075e0
NisemonoQ <yann.badinga@gmail.com>
2024-04-14 19:54:06 +0200

    random de trop enlevé

--- Assets/Scripts/Nouveau Projet/LevelSystem/Collectibles/DropRateManager.cs
+++ Assets/Scripts/Nouveau Projet/LevelSystem/Collectibles/DropRateManager.cs
@@ -17,21 +17,22 @@ public class DropRateManager : MonoBehaviour
     public void TryDrop()
     {
         float randomNmb = Random.Range(0f, 100f);
-        List<Drops> possibility = new List<Drops>();
+        //List<Drops> possibility = new List<Drops>();
 
         foreach(Drops rate in drop)
         {
             if(randomNmb <= rate.dropRate)
             {
-                possibility.Add(rate);
+                //  possibility.Add(rate);
+                Instantiate(rate.itemPrefab, transform.position, Quaternion.identity);
+
             }
         }
 
-        if(possibility.Count > 0)
+        /*if(possibility.Count > 0)
         {
             Drops drops = possibility[Random.Range(0, possibility.Count)];
-            Instantiate(drops.itemPrefab, transform.position, Quaternion.identity);
-        }
+        }*/
 
     }
 }
