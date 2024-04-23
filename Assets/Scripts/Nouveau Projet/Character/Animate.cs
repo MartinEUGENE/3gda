4947ff8877f2f8d941cd1a553776fa1f92ed7388
NisemonoQ <yann.badinga@gmail.com>
2024-04-23 19:19:15 +0200

    Update, Animations and a few sprites integrated

--- Assets/Scripts/Nouveau Projet/Character/Animate.cs
+++ Assets/Scripts/Nouveau Projet/Character/Animate.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class Animate : MonoBehaviour
 {
-    Animator anim;
-    public float hoerizontal;
+    Animator animate;
+    CharactControls ch;
+    SpriteRenderer sp; 
     void Start()
     {
-        anim = GetComponent<Animator>();
+        animate = GetComponent<Animator>();
+        ch = GetComponent<CharactControls>();
+        sp = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        anim.SetFloat("Horizontal", hoerizontal);
+        if(ch.moving.x !=0 || ch.moving.y != 0)
+        {
+            animate.SetBool("Move", true);
+            SpriteFlipper();
+        }
+        else
+        {
+            animate.SetBool("Move", false);
+
+        }
+    }
+
+    void SpriteFlipper()
+    {
+        if(ch.lastMovHorizon < 0)
+        {
+            sp.flipX = true;
+        }
+        else
+        {
+            sp.flipX = false; 
+        }
+
     }
 }
