6373346cda1bd343440788dd26ecde89eb35d7c6
NisemonoQ <yann.badinga@gmail.com>
2024-05-29 23:16:40 +0200

    WIP on main : today

--- Assets/Scripts/Nouveau Projet/Enemies/SpecialEnemies/RushingEnemy.cs
+++ Assets/Scripts/Nouveau Projet/Enemies/SpecialEnemies/RushingEnemy.cs
@@ -11,7 +11,9 @@ public class RushingEnemy : EnemiesSystem
 
     Vector2 playerDir;
 
-
+    Animator animate;
+    CharactControls ch;
+    SpriteRenderer sp;
 
     int dist = 15; 
     public float rushForce; 
@@ -22,6 +24,12 @@ public class RushingEnemy : EnemiesSystem
     public override void Awake()
     {
         base.Awake();
+
+        //animate.SetBool("IsMoving", false);
+
+        animate = GetComponent<Animator>();
+        ch = FindObjectOfType<CharactControls>();
+        sp = GetComponent<SpriteRenderer>();
     }
     public override void OnSpawn()
     {
@@ -43,10 +51,31 @@ public class RushingEnemy : EnemiesSystem
         playerDir = playerObj.transform.position - transform.position;
         prediction.SetPosition(1, 5 * playerDir);
 
+        if(animate.GetBool("IsMoving") == false)
+        {
+            if(ch.moving.x != 0 || ch.moving.y != 0)
+            {
+                SpriteFlipper(); 
+            }
+        }
+
         if (dead == true)
         {
             Die();
         }
+    }
+
+    void SpriteFlipper()
+    {
+        if (ch.lastMovHorizon < 0)
+        {
+            sp.flipX = true;
+        }
+        else
+        {
+            sp.flipX = false;
+        }
+
     }
 
     public IEnumerator Rush()
@@ -58,6 +87,8 @@ public class RushingEnemy : EnemiesSystem
         yield return new WaitForSeconds(.1f);*/
         yield return new WaitForSeconds(startingTime - .2f);
         prediction.enabled = false;
+        animate.SetBool("IsMoving", true);
+
         rb2d.velocity = new Vector2(playerDir.x, playerDir.y).normalized * rushForce;
         yield return new WaitForSeconds(rushDist);
                
